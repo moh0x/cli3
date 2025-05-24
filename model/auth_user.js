850858b053f44e0fd66b69cfb3fa5ec1f3858a79
moh0x <mihoyahou@gmail.com>
2025-05-24 13:23:14 +0000

    first commit

---
+++ model/auth_user.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema(
+	{
+		username:{
+			type: String,
+			
+		},
+		email: {
+			type: String,
+			required: true,
+	
+		},
+		password: {
+			type: String,
+			required: true,
+			minLength: 8,
+		},
+		phoneNumber: {
+			type: String,
+			default:null,
+		
+		},
+		token:{
+			type:String,
+			default:null
+		},
+        role:{
+            type:String,
+            required:true,
+            maxlength:10,
+			enum:['eng']
+        },
+	},
+	
+	{ timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+module.exports = {User}
---
+++ model/payments_model.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+const paymentSchema = new mongoose.Schema(
+    {
+        userId:{
+             type: mongoose.Schema.Types.ObjectId,
+                        required: true,
+                        ref: "User",
+        },
+       chauferName:{
+        type:String,
+       },
+       numberOfCourses:{
+        type:Number
+       },
+       chauferPay:{
+        type:Number
+       },
+       status:{
+        type:String,
+        enum:['start','finish'],
+        default:"finish"
+       },
+       tax:{
+			type:String,
+			default:0
+		},
+
+    },
+    { timestamps: true }
+);
+
+const Payment = mongoose.model("Payment", paymentSchema);
+module.exports = {Payment}
